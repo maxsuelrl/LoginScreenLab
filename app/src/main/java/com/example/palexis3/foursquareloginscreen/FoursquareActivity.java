2efcd00b14d3040f293b485c01fe7fd0989e7e24
palexis3 <alexispatrick77@gmail.com>
2016-10-14 13:49:57 -0700

    First Commit

---
+++ app/src/main/java/com/example/palexis3/foursquareloginscreen/FoursquareActivity.java
@@ -0,0 +1,13 @@
+package com.example.palexis3.foursquareloginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoursquareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foursquare);
+    }
+}
